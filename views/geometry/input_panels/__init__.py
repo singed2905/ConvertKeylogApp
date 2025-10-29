3945ee0a3c645d6fb239c624eced47c4076f777e
Đặng Vũ Hưng <88934176+singed2905@users.noreply.github.com>
2025-10-29 15:47:03 +0700

    Create input panels package init

---
+++ views/geometry/input_panels/__init__.py
@@ -0,0 +1,11 @@
+"""Geometry Input Panels Package"""
+
+# Import các panel components
+from .geometry_input_manager import GeometryInputManager
+
+__all__ = [
+    'GeometryInputManager'
+]
+
+__version__ = '2.0.0'
+__description__ = 'Input panel components for 5 geometry shapes'
