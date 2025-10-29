9c70fb85831bc576770be17cae453f36f8a1f0f2
Đặng Vũ Hưng <88934176+singed2905@users.noreply.github.com>
2025-10-29 08:33:40 +0700

    Update views/__init__.py to export all views

--- views/__init__.py
+++ views/__init__.py
@@ -2,5 +2,8 @@
 # Contains all user interface components
 
 from .main_view import MainView
+from .geometry_view import GeometryView
+from .equation_view import EquationView
+from .polynomial_equation_view import PolynomialEquationView
 
-__all__ = ['MainView']
+__all__ = ['MainView', 'GeometryView', 'EquationView', 'PolynomialEquationView']
