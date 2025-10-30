c0476bcf1623db8da6b0694ab007a77d453a2fef
Đặng Vũ Hưng <88934176+singed2905@users.noreply.github.com>
2025-10-30 08:33:13 +0700

    Create equation service package init

---
+++ services/equation/__init__.py
@@ -0,0 +1,10 @@
+"""Equation Service Package"""
+
+from .equation_service import EquationService
+
+__all__ = [
+    'EquationService'
+]
+
+__version__ = '2.0.0'
+__description__ = 'Linear equation system solving with keylog encoding'
